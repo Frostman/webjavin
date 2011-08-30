57ef7ef339877f871abc3c030d71b186b4f2a08e
slukjanov <me@frostman.ru>
2011-08-30 23:50:31 +0400

    Base MVEL2 expression compilation and execution added.

--- webjavin-core/src/main/java/ru/frostman/web/aop/MethodPatternMatcher.java
+++ webjavin-core/src/main/java/ru/frostman/web/aop/MethodPatternMatcher.java
@@ -18,19 +18,65 @@
 
 package ru.frostman.web.aop;
 
+import com.google.common.collect.Maps;
 import javassist.CtMethod;
+import org.mvel2.MVEL;
+import org.mvel2.ParserContext;
+import org.mvel2.compiler.ExecutableStatement;
+import ru.frostman.web.thr.JavinRuntimeException;
+
+import java.lang.reflect.Array;
+import java.util.Map;
 
 /**
  * @author slukjanov aka Frostman
  */
 public class MethodPatternMatcher {
-    private final String methodPattern;
+    private static final ThreadLocal<CtMethod> currentMethod = new ThreadLocal<CtMethod>();
+    private static final Map<String, Object> vars = Maps.newHashMap();
+
+    static {
+        vars.put("any", true);
+    }
+
+    private final ExecutableStatement expression;
+
+    // package('ru.frostman*') && class('*') && method('*') && return('*') && annotation ()
 
     public MethodPatternMatcher(String methodPattern) {
-        this.methodPattern = methodPattern;
+        try {
+            ParserContext context = new ParserContext();
+            context.setStrongTyping(true);
+
+            context.addInput("any", boolean.class);
+
+            Class<?> stringArrClass = Array.newInstance(String.class, 0).getClass();
+            context.addImport("package", MethodPatternMatcher.class.getMethod("_package", stringArrClass));
+
+            if (methodPattern.trim().length() == 0) {
+                methodPattern = "any";
+            }
+            expression = (ExecutableStatement) MVEL.compileExpression(methodPattern, context);
+
+            if (expression.getKnownEgressType() != boolean.class && expression.getKnownEgressType() != Boolean.class) {
+                throw new JavinRuntimeException("Aop method interceptor pattern should return boolean");
+            }
+        } catch (Exception e) {
+            throw new JavinRuntimeException("Exception while compiling method interceptor patern: " + methodPattern, e);
+        }
     }
 
     public boolean matches(CtMethod method) {
-        return methodPattern.length() == 0 || method.getName().equals(methodPattern);
+        try {
+            currentMethod.set(method);
+
+            return (Boolean) MVEL.executeExpression(expression, vars);
+        } finally {
+            currentMethod.set(null);
+        }
+    }
+
+    public static boolean _package(String... packages) {
+        return true;
     }
 }
